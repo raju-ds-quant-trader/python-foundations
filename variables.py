c39d2f76c8bd3bb819733ba165c73d16b54fa611
Gugulothu Raju <97868664+raju-ds-quant-trader@users.noreply.github.com>
2025-12-20 09:29:27 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,30 @@
+a,b,c=1,2,3
+print(a,b,c)
+d=1  #int
+e=3.14 #float
+f="raju" #string
+g=True #boolen
+
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+#re assigning variables
+count=5
+count=count+1
+print(count) #this prints 6 as value increased by 1
+#python is case sensitive
+age=30
+Age=40
+print(age,Age)
+#input funcction to take inpur from user
+a=input("enter a number")
+print(a)
+#type casting onverting one data type to another
+x=10
+y=int(x)
+print(y+5)
+# variable deletion with del fun
+del x
+del y  # deletes x,y in 
+
