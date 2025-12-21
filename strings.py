07b0c15a3d76038885fe7c2fe3a04067b2e4fa73
Gugulothu raju <Raju.vasu945@gmail.com>
2025-12-21 17:38:06 +0530

    practise

---
+++ strings.py
@@ -0,0 +1,10 @@
+string="i love you yashaswi nayak beta"
+string1=string.replace("yashaswi","abhi ram")
+print (string1)
+print(string)
+string3="i love you priya"
+print(string3*3)
+string4=string3.upper()
+print(string4*6)
+love_message=string1+""+string
+print(love_message)
