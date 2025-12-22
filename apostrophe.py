48989b0ede7138a99922af250b68c04676e6800c
Gugulothu raju <Raju.vasu945@gmail.com>
2025-12-22 20:21:44 +0530

    apostrophe practise

---
+++ apostrophe.py
@@ -0,0 +1,7 @@
+message="one of python's strength is its diverse community"
+# does not show any error as apostrophe is wiht in inverted commas
+message1="one of python's is its diverse community"
+#it shows syntax error as apostrophe is not handled properly
+print ("hello python world 2.7")
+quote="albert eintine said a person who nver made a mistake \n never tried anything"
+print(quote)
