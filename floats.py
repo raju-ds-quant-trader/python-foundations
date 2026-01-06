78828033372d19879a6a6bfb01097110b17e3d5c
Gugulothu raju <Raju.vasu945@gmail.com>
2026-01-06 10:51:41 +0530

    Added numbers and floats programs

---
+++ floats.py
@@ -0,0 +1,10 @@
+a=0.12+0.32
+b=0.23+0.45
+c=12.31-10.12
+d=3.14*2.0
+e=5.0/2.2
+f=7.5*1.2-2.14*1.23
+print(a,b,c,d,e,f)
+#floats aritthmetic operations can give you some times arbitarage values
+g=0.2+0.1      
+print(g)
---
+++ numbers.py
@@ -0,0 +1,17 @@
+a=2
+b=3
+c=a*b
+d=a+b
+f=a-b
+print("multipliation is = " ,c)
+# to find power of a number two astricks are used
+g=a**b
+print("a to the power b is=",g)
+#python supports order of operations also
+e=2*3
+f=(2*3)+4
+print("the operations as =",e,f)
+#the spacing in between the operators and operands does not matter in above operations
+#python cals any deimal number as float
+g=0.12+0.13*0.23/0.1-0.2
+print("the simplification of above numbers is =",g)
