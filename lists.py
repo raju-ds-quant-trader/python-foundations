022efb8f6d28d43291ac4d1e22f60480667210f7
Gugulothu raju <Raju.vasu945@gmail.com>
2026-01-21 13:17:52 +0530

    Updated lists.py

---
+++ lists.py
@@ -0,0 +1,11 @@
+#list allow you to store lot of information in a dingle variable
+# you can store letters,numbers,words and more in a list
+# here is a sample list
+bycyles=['treck','cannondate','redline','specialised']
+print(bycyles)
+bycycles_2= "atlas","ranger","hero"
+print(bycycles_2)
+#python prints list values with in brackets [] ,and you can access any item from the list
+print(bycycles_2[0])
+print(bycycles_2[1])
+print(bycycles_2[0:3])
